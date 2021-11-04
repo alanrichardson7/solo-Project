b1f5b66a583a261125cd64971b443b54a04842cb
Alan Richardson <alanrichardson723@gmail.com>
2021-11-04 16:05:24 -0400

    working with multiple fields

--- client/components/SearchDisplay.jsx
+++ client/components/SearchDisplay.jsx
@@ -3,20 +3,49 @@ import React, { Component } from 'react';
 class SearchDisplay extends Component {
     constructor(props){
         super(props)
+        this.state = {
+            numOfFields = 1
+        };
     };
 
-    // refCreator(){
-    //     const ref = React.createRef;
-    //     return ref;
-    // };
+    // what if i created a button next to the submit button called 'add another instrument'
+    // when clicked, it would 
+
+    renderField2(){
+        this.setState({show2: true})
+    }
+
+    renderField3(){
+        this.setState({show3: true})
+    }
+
+    renderField4(){
+        this.setState({show4: true})
+    }
+
+    renderField5(){
+        this.setState({show5: true})
+    }
+
+    // initialize an empty array
+    // create another button in the form that on click, calls the function below
+        // create a function that while array.length is less than 10 creates a new input field
 
     render(){
         return(
             <div id="searchDisplay">
                 <form action='/search'>
                     <p id="searchInstructions">Enter instrument name in 'Intrument' field, and number of that instrument in 'Number' field</p> <br></br>
-                    <input type="text" id="searchField" name="instrumentField" placeholder="Instrument"></input>
-                    <input type="text" id="searchField" name="instrumentNumber" placeholder="Number"></input><br></br>
+                    <input type="text" id="searchField" name="instrumentField1" placeholder="Instrument 1"></input>
+                    <input type="text" id="searchField" name="instrumentNumber1" placeholder="Number"></input><br></br>
+                    <input type="text" id="searchField" name="instrumentField2" placeholder="Instrument 2"></input>
+                    <input type="text" id="searchField" name="instrumentNumber2" placeholder="Number"></input><br></br>
+                    <input type="text" id="searchField" name="instrumentField3" placeholder="Instrument 3"></input>
+                    <input type="text" id="searchField" name="instrumentNumber3" placeholder="Number"></input><br></br>
+                    <input type="text" id="searchField" name="instrumentField4" placeholder="Instrument 4"></input>
+                    <input type="text" id="searchField" name="instrumentNumber4" placeholder="Number"></input><br></br>
+                    <input type="text" id="searchField" name="instrumentField5" placeholder="Instrument 5"></input>
+                    <input type="text" id="searchField" name="instrumentNumber5" placeholder="Number"></input><br></br>
                     <input type="submit" value="Submit" id="button"></input>
                 </form>
             </div>
--- server/controllers/compositionController.js
+++ server/controllers/compositionController.js
@@ -6,10 +6,32 @@ const compositionController = {};
 
 compositionController.findComposition = (req, res, next) => {
     try {
-        const instrument = req.query.instrumentField;
-        const number = req.query.instrumentNumber;
-        const query = {}
-        query[instrument] = number;
+        const query = {};
+        if (req.query.instrumentField1){
+            const instrument = req.query.instrumentField1;
+            const number = req.query.instrumentNumber1;
+            query[instrument] = number;
+        }
+        if (req.query.instrumentField2){
+            instrument = req.query.instrumentField2;
+            number = req.query.instrumentNumber2;
+            query[instrument] = number;
+        }
+        if (req.query.instrumentField3){
+            instrument = req.query.instrumentField3;
+            number = req.query.instrumentNumber3;
+            query[instrument] = number;
+        }
+        if (req.query.instrumentField4){
+            instrument = req.query.instrumentField4;
+            number = req.query.instrumentNumber4;
+            query[instrument] = number;
+        }
+        if (req.query.instrumentField5){
+            instrument = req.query.instrumentField5;
+            number = req.query.instrumentNumber5;
+            query[instrument] = number;
+        }
         db.find(query)
             .then(data => {
                 res.locals.result = data;
